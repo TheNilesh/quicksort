db6fa03433dc407d7829d355287af14bc35e9163
Nilesh Akhade <akhadenilesh93@gmail.com>
2014-09-16 15:13:46 +0530

    ok but no output

--- source/main.c
+++ source/main.c
@@ -1,11 +1,16 @@
-#include<stdio>
+#include<stdio.h>
 
 int main()
 {
 int i,j,n,temp;
-int arr[]={2,5,1,6,3,10,2};
+int arr[7];
 n=7;
-for(i=0<i<n;i++)
+
+for(i<0;i<n;i++)
+	printf("%d  ", arr[i]);
+
+printf("Sorting program");
+for(i=0;i<n;i++)
 	for(j=i;j<n-1;j++)
 		if(arr[j]<arr[j+1])
 		{
