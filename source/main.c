3d5a5f871da9583c086595acad09323c0ea959c4
Nilesh Akhade <akhadenilesh93@gmail.com>
2014-09-16 14:36:25 +0530

    first save bubble inside

---
+++ source/main.c
@@ -0,0 +1,21 @@
+#include<stdio>
+
+int main()
+{
+int i,j,n,temp;
+int arr[]={2,5,1,6,3,10,2};
+n=7;
+for(i=0<i<n;i++)
+	for(j=i;j<n-1;j++)
+		if(arr[j]<arr[j+1])
+		{
+		 temp=arr[j];
+		 arr[j]=arr[j+1];
+		 arr[j+1]=temp;
+		}
+
+for(i<0;i<n;i++)
+	printf("%d  ", arr[i]);
+			
+return 0;
+}
